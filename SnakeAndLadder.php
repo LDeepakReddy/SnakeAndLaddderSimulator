0944326ef7c91fab9b17291ee4174b0030c7af67
LDeepakReddy <l.deepakreddy.com>
2022-06-10 22:59:58 +0530

    Added : Function to initialise the player position

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,6 +1,17 @@
 <?php
+class SnakeAndLadder{
 
-echo "Welcome to the Snake And Ladder Simulator Program"
+function welcome(){
+echo "Welcome to the Snake And Ladder Simulator Program";
+}
 
+function startPosition(){
+    $startPosition = 0;
+    echo "Player started at a $startPosition position";
+}
+
+}
+$position = new SnakeAndLadder();
+$position->startPosition();
 
 ?>
